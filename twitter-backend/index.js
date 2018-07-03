d57be9881bac1f6d357861614a7950284488c40b
sagardesai90 <saagardesai@gmail.com>
2018-07-02 20:18:36 -0500

    added stream so I tweet back at followers

--- twitter-backend/index.js
+++ twitter-backend/index.js
@@ -12,14 +12,17 @@ var params = {
     count: 10
 }
 
-T.get('search/tweets', params, gotData);
-function gotData(err, data, response){
-    var tweets = data.statuses;
-    for(var i = 0; i < tweets.length; i++){
-        console.log(tweets[i].text);
-        console.log(tweets[i].coordinates);
-    }
-}
+// T.get('search/tweets', params, gotData);
+// function gotData(err, data, response){
+//     var tweets = data.statuses;
+//     for(var i = 0; i < tweets.length; i++){
+//         console.log(tweets[i].text);
+//         console.log(tweets[i].coordinates);
+//     }
+// }
+
+//You can't tweet the same status via the console.. 
+//I guess it's to prevent spam!
 let tweet = {
     status: 'tweeting from the console via twitter api'
 }
@@ -29,5 +32,30 @@ function tweeted(err, data, response){
         console.log('something went wrong, try again');
     } else {
         console.log('it worked!');
+    }
+};
+
+//setting up a user stream
+let stream = T.stream('user');
+//anytime someone follows our account
+stream.on('follow', followed);
+
+function followed(eventMsg){
+    let name = eventMsg.source.name;
+    let screenName = eventMsg.source.screen_name;
+    tweetIt('@' + screenName + ' thanks for following, we will share food truck locations with you!');
+}
+function tweetIt(txt){
+    let tweet = {
+        status: txt
+    }
+    T.post('statuses/update', tweet, tweeted);
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log('something went wrong, try again');
+        } else {
+            console.log('it worked!');
+        }
     }
 }
