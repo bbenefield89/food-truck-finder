3955e6b3337d8c5ccbbf41097da19842091a7b9f
sagardesai90 <saagardesai@gmail.com>
2018-07-03 00:23:58 -0500

    added function to get coordinates in lat, lon format which is what google and apple maps accept

--- twitter-backend/index.js
+++ twitter-backend/index.js
@@ -8,18 +8,22 @@ console.log(config);
 var T = new Twit(config);
 
 var params = {
-    q: 'bobaepapa',
+    q: 'foodtruckfinde1',
     count: 10
 }
 
-// T.get('search/tweets', params, gotData);
-// function gotData(err, data, response){
-//     var tweets = data.statuses;
-//     for(var i = 0; i < tweets.length; i++){
-//         console.log(tweets[i].text);
-//         console.log(tweets[i].coordinates);
-//     }
-// }
+T.get('search/tweets', params, gotData);
+function gotData(err, data, response){
+    var tweets = data.statuses;
+    let coords = [];
+    for(var i = 0; i < tweets.length; i++){
+        if(tweets[i].coordinates){
+            coords.push(tweets[i].coordinates);
+        }
+    }
+    coords.forEach( coord => coord.coordinates.reverse())
+    console.log(coords);
+}
 
 //You can't tweet the same status via the console.. 
 //I guess it's to prevent spam!
@@ -36,26 +40,26 @@ function tweeted(err, data, response){
 };
 
 //setting up a user stream
-let stream = T.stream('user');
-//anytime someone follows our account
-stream.on('follow', followed);
+// let stream = T.stream('user');
+// //anytime someone follows our account
+// stream.on('follow', followed);
 
-function followed(eventMsg){
-    let name = eventMsg.source.name;
-    let screenName = eventMsg.source.screen_name;
-    tweetIt('@' + screenName + ' thanks for following, we will share food truck locations with you!');
-}
-function tweetIt(txt){
-    let tweet = {
-        status: txt
-    }
-    T.post('statuses/update', tweet, tweeted);
+// function followed(eventMsg){
+//     let name = eventMsg.source.name;
+//     let screenName = eventMsg.source.screen_name;
+//     tweetIt('@' + screenName + ' thanks for following, we will share food truck locations with you!');
+// }
+// function tweetIt(txt){
+//     let tweet = {
+//         status: txt
+//     }
+//     T.post('statuses/update', tweet, tweeted);
 
-    function tweeted(err, data, response){
-        if(err){
-            console.log('something went wrong, try again');
-        } else {
-            console.log('it worked!');
-        }
-    }
-}
+//     function tweeted(err, data, response){
+//         if(err){
+//             console.log('something went wrong, try again');
+//         } else {
+//             console.log('it worked!');
+//         }
+//     }
+// }
