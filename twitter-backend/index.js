165aae2b245d0f2c7e4c68a410e558b2a32ee154
sagardesai90 <saagardesai@gmail.com>
2018-07-03 02:02:13 -0500

    added a reply to any @tweet

--- twitter-backend/index.js
+++ twitter-backend/index.js
@@ -21,26 +21,28 @@ function gotData(err, data, response){
             coords.push(tweets[i].coordinates);
         }
     }
-    coords.forEach( coord => coord.coordinates.reverse())
-    console.log(coords);
+    coords.forEach( coord => coord.coordinates.reverse());
+    // coords.forEach(coord => coord.coordinates);
+    // console.log(coords);
 }
 
 //You can't tweet the same status via the console.. 
 //I guess it's to prevent spam!
-let tweet = {
-    status: 'tweeting from the console via twitter api'
-}
-T.post('statuses/update', tweet, tweeted);
-function tweeted(err, data, response){
-    if(err){
-        console.log('something went wrong, try again');
-    } else {
-        console.log('it worked!');
-    }
-};
+// let tweet = {
+//     status: 'tweeting from the console via twitter api'
+// }
+// T.post('statuses/update', tweet, tweeted);
+
+// function tweeted(err, data, response){
+//     if(err){
+//         console.log('something went wrong, try again');
+//     } else {
+//         console.log('it worked!');
+//     }
+// };
 
 //setting up a user stream
-// let stream = T.stream('user');
+let stream = T.stream('user');
 // //anytime someone follows our account
 // stream.on('follow', followed);
 
@@ -49,17 +51,36 @@ function tweeted(err, data, response){
 //     let screenName = eventMsg.source.screen_name;
 //     tweetIt('@' + screenName + ' thanks for following, we will share food truck locations with you!');
 // }
-// function tweetIt(txt){
-//     let tweet = {
-//         status: txt
-//     }
-//     T.post('statuses/update', tweet, tweeted);
 
-//     function tweeted(err, data, response){
-//         if(err){
-//             console.log('something went wrong, try again');
-//         } else {
-//             console.log('it worked!');
-//         }
-//     }
-// }
+stream.on('tweet', tweetEvent);
+function tweetEvent(eventMsg){
+    let replyTo = eventMsg.in_reply_to_screen_name;
+    let text = eventMsg.text;
+    let from = eventMsg.user.screen_name;
+
+    console.log(replyTo + ' ' + from);
+    if(replyTo === 'foodtruckfinde1' ){
+        console.log("hello there, looks like we've got engagement");
+        console.log(eventMsg);
+        let newTweet = '@' + from + ' thank you for signing up and sharing your food trucks location!';
+        tweetIt(newTweet);
+    }
+}
+
+function tweetIt(txt){
+    let tweet = {
+        status: txt
+    }
+    T.post('statuses/update', tweet, tweeted);
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log('something went wrong, try again');
+        } else {
+            console.log('it worked!');
+        }
+        console.log("hello there, looks like we've got engagement");
+    }
+}
+
+ 
