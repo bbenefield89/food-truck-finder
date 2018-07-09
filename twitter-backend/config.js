4984ae9ea1a15cae0e9294a055ffc910ca0723f2
Sagar Desai <saagardesai@gmail.com>
2018-07-09 01:04:23 -0500

    Delete config.js

--- twitter-backend/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    consumer_key:         'KTzqqKfR9DMDJRxdwvrP69VrM',
-    consumer_secret:      '2x0hxaAGJIhS2VbjkSz3uupHmYcDViZCF2rhqIVg5OcYv9Aeqe',
-    access_token:         '1013860593738997760-f4AMXO6OW7KuE8iytCKkTKAyzYfXnH',
-    access_token_secret:  'eak1PwMkpgsw6Xh40NncE61JMZ3dhE9GAVEDIZbLH8lhF',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  }
